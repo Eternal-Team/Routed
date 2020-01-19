5b27c604ec1a973a6821668d1c1e3da339b2ac0d
Itorius <itorius.yt@gmail.com>
2020-01-19 16:26:28 +0100

    Provider Module modes

--- Modules/ProviderModule.cs
+++ Modules/ProviderModule.cs
@@ -1,9 +1,11 @@
 ﻿using BaseLibrary;
 using BaseLibrary.UI;
+using ContainerLibrary;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Routed.Layer;
 using System;
+using Terraria;
 using Terraria.Audio;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -11,8 +13,15 @@ using Terraria.ModLoader.IO;
 
 namespace Routed.Modules
 {
-	public class ProviderModule : BaseModule
+	public class ProviderModule : BaseModule, IHasUI
 	{
+		public enum Mode
+		{
+			TakeAll,
+			LeaveStack,
+			Leave1
+		}
+
 		public override int DropItem => ModContent.ItemType<Items.ProviderModule>();
 
 		public Guid UUID { get; set; }
@@ -20,9 +29,42 @@ namespace Routed.Modules
 		public LegacySoundStyle CloseSound => SoundID.Item1;
 		public LegacySoundStyle OpenSound => SoundID.Item1;
 
+		public Mode mode = Mode.TakeAll;
+
 		public ProviderModule()
 		{
 			UUID = Guid.NewGuid();
+		}
+
+		public int GetAvailableItems(int type)
+		{
+			int count = CachedHandler.CountItems(type);
+			switch (mode)
+			{
+				case Mode.Leave1:
+					return count - 1;
+				case Mode.LeaveStack:
+				{
+					int max = 0;
+					for (int i = 0; i < CachedHandler.Slots; i++)
+					{
+						Item item = CachedHandler.GetItemInSlot(i);
+						if (item.IsAir || item.type != type) continue;
+						if (item.stack > max) max = item.stack;
+					}
+
+					return count - max;
+				}
+				default:
+					return count;
+			}
+		}
+
+		public override bool Interact()
+		{
+			if (!Main.dedServ) PanelUI.Instance.HandleUI(this);
+
+			return true;
 		}
 
 		public override void Draw(SpriteBatch spriteBatch)
@@ -36,11 +78,13 @@ namespace Routed.Modules
 			base.Load(tag);
 
 			UUID = tag.Get<Guid>("UUID");
+			mode = (Mode)tag.GetInt("Mode");
 		}
 
 		public override TagCompound Save() => new TagCompound
 		{
-			["UUID"] = UUID
+			["UUID"] = UUID,
+			["Mode"] = (int)mode
 		};
 	}
 }
---
+++ UI/ProviderModulePanel.cs
@@ -0,0 +1,55 @@
+﻿using BaseLibrary.Input.Mouse;
+using BaseLibrary.UI;
+using Microsoft.Xna.Framework;
+using Routed.Modules;
+using Terraria;
+
+namespace Routed.UI
+{
+	public class ProviderModulePanel : BaseUIPanel<ProviderModule>
+	{
+		public ProviderModulePanel(ProviderModule module) : base(module)
+		{
+			Width.Pixels = 200;
+			Height.Pixels = 64;
+
+			UIText textLabel = new UIText("Provider Module")
+			{
+				Width = { Percent = 100 },
+				Height = { Pixels = 20 },
+				X = { Percent = 50 },
+				HorizontalAlignment = HorizontalAlignment.Center
+			};
+			Add(textLabel);
+
+			UITextButton buttonClose = new UITextButton("X")
+			{
+				Size = new Vector2(20),
+				X = { Percent = 100 },
+				Padding = Padding.Zero,
+				RenderPanel = false
+			};
+			buttonClose.OnClick += args => PanelUI.Instance.CloseUI(Container);
+			Add(buttonClose);
+
+			UIText textMode = new UIText(module.mode.ToString())
+			{
+				Width = { Percent = 100 },
+				Height = { Pixels = 20 },
+				X = { Percent = 50 },
+				Y = { Pixels = 28 },
+				HorizontalAlignment = HorizontalAlignment.Center
+			};
+			textMode.OnMouseDown += args =>
+			{
+				if (args.Button != MouseButton.Left) return;
+
+				args.Handled = true;
+
+				module.mode = module.mode.NextEnum();
+				textMode.Text = module.mode.ToString();
+			};
+			Add(textMode);
+		}
+	}
+}
